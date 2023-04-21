91cbc3439f2b20741067494ffb0a3d4bbaf2aa51
Frederik Baun Grubak <fred00s7@edu.zealand.dk>
2023-04-21 11:21:49 +0200

    Lavede rettelser

--- TestGithub23/Program.cs
+++ TestGithub23/Program.cs
@@ -5,12 +5,5 @@ Console.WriteLine("Hello, World!");
 ClassF f = new ClassF(99, "Frederik");
 Console.WriteLine(f.Name + " " + f.Age);
 
-using TestGithub23;
-
-Console.WriteLine("Hello, World!");
-
-ClassF F = new ClassF(25, "F");
-Console.WriteLine($"Name: {F.Name}");
-
 Daniel D = new Daniel("Daniel", "Vejvej22");
 Console.WriteLine($"Name: {D.Name}, Address: {D.Address}.");

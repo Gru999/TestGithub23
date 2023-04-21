04d0116789c4ef00119fd4b29c6146df9f1db1e0
Frederik Baun Grubak <fred00s7@edu.zealand.dk>
2023-04-21 10:58:34 +0200

    ClassF lavet

---
+++ TestGithub23/ClassF.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGithub23
+{
+    public class ClassF
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public ClassF(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+
+
+
+    }
+}

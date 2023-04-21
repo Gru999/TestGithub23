00901ad24ed4fd02ca3bd3793f5271c10daf9ba2
Daniel Hass <dani67h0@edu.zealand.dk>
2023-04-21 11:11:50 +0200

    ClassTest

--- TestGithub23/Program.cs
+++ TestGithub23/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TestGithub23;
+
+Console.WriteLine("Hello, World!");
+
+ClassF F = new ClassF(25, "F");
+Console.WriteLine($"Name: {F.Name}");
+
+Daniel D = new Daniel("Daniel", "Vejvej22");
+Console.WriteLine($"Name: {D.Name}, Address: {D.Address}.");
